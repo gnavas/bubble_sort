fb8388356b279649b97fc3a8c328d7d26174baef
George Navas <gannavas@gmail.com>
2015-03-16 16:41:53 -0700

    Ruby solution

--- lib/sort.rb
+++ lib/sort.rb
@@ -1,6 +1,29 @@
 class Array
 
-  def bubble_sort
-  end
+arr = [4, 5, 34, 72, 17, 2]
 
+def swap arr
+ count = 0
+ while (count < arr.length - 1) do
+   first = arr[count]
+   second = arr[count + 1]
+   if first > second
+     arr[count + 1] = first
+     arr[count] = second
+     count += 1
+   else
+     count +=1
+   end
+ end
+ arr
+end
+
+def bubble_sort arr
+ counter = arr.length
+ while (counter > 0) do
+   swap arr
+   counter -= 1
+ end
+ arr
+end
 end
--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -3,18 +3,22 @@ require_relative '../lib/sort'
 
 describe "bubble_sort" do
   it "sorts the array with a simple order" do
-      expect([5,3,2,1,4].bubble_sort).to eq([1,2,3,4,5])
+     arr = [5,3,2,1,4]
+      expect(arr.bubble_sort arr).to eq([1,2,3,4,5])
   end
 
   it "sorts the array with negative numbers" do
-      expect([-5,3,-2,1,-4].bubble_sort).to eq([-5,-4,-2,1,3])
+      arr = [-5,3,-2,1,-4]
+      expect(arr.bubble_sort arr).to eq([-5,-4,-2,1,3])
   end
 
   it "sorts the array with floats" do
-      expect([1.5,3.4,2,1,4].bubble_sort).to eq([1,1.5,2,3.4,4])
+      arr = [1.5,3.4,2,1,4]
+      expect(arr.bubble_sort arr).to eq([1,1.5,2,3.4,4])
   end
 
   it "sorts the array with a randomized order" do
-      expect((1..10).to_a.shuffle.bubble_sort).to eq((1..10).to_a)
+      arr = (1..10).to_a.shuffle
+      expect(arr.bubble_sort arr).to eq((1..10).to_a)
   end
 end
